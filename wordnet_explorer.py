7f31a56e1030a4fb51fa9c4d615067871f8c27c9
Brennan Waters <brennanwaters34@gmail.com>
2018-03-11 20:55:24 -0400

    adding semcor explorer (how many words in semcor are descended from food?)

--- wordnet_explorer.py
+++ wordnet_explorer.py
@@ -10,14 +10,12 @@ def get_hypernym_names_from_list_of_paths(list_of_paths):
     return synset_names_set
 
 
-def descendant_of_food(word, setting = 'most_common', ):
+def string_is_descendant_of_food(word, setting = 'most_common', ):
     """
     :param word:
     :param setting: parameters = 'most_common', 'majority_vote' and 'all'
     :return: True if the word is a descendant of "food" in WordNet 3.0, False if not
     """
-    if word == 'beef stew':
-        print("GOT")
     food_synset_names = [syn.name() for syn in wn.synsets('food')]
     synset_list = wn.synsets(word)
     just_nouns = list(filter(lambda x: x.pos() == 'n', synset_list))
@@ -31,14 +29,20 @@ def descendant_of_food(word, setting = 'most_common', ):
     # else:
     #     raise ValueError
     for synset in just_nouns:
-        path_names = get_hypernym_names_from_list_of_paths(synset.hypernym_paths())
-        for food_synset_name in food_synset_names:
-            # print(path_names)
-            # print(food_synset_name)
-            if food_synset_name in path_names:
-                print(path_names)
-                print(food_synset_name)
-                return True
+        if synset_is_descendant_of_food(synset):
+            return True
+    return False
+
+def synset_is_descendant_of_food(synset):
+    food_synset_names = [syn.name() for syn in wn.synsets('food')]
+    path_names = get_hypernym_names_from_list_of_paths(synset.hypernym_paths())
+    for food_synset_name in food_synset_names:
+        # print(path_names)
+        # print(food_synset_name)
+        if food_synset_name in path_names:
+            print(path_names)
+            print(food_synset_name)
+            return True
     return False
 
 if __name__ == "__main__":
